c0f34eb97df000c71a1bc7ff26d0dca5031eb0ed
alealonso17 <2alejandroalonso70@gmail.com>
2025-12-29 14:04:13 +0100

    Initial Commit

---
+++ DeepFocus/DeepFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepFocusApp.swift
+//  DeepFocus
+//
+//  Created by Alejandro Alonso on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
